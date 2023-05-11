1fc4f3fccaea3233d1ca1976dcb4bf9fd3f081a1
mappii130 <90373590+mappii130@users.noreply.github.com>
2023-05-11 15:10:16 +0900

    categry_homeへデータを表示

--- django/categories/views.py
+++ django/categories/views.py
@@ -5,8 +5,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 import json
-from .models import Category
+from .models import Category, ActivityCategory
 from .forms import CategoryForm
+from datetime import timedelta, date
 
 # TODO: ホーム画面にカテゴリー毎の累計活動時間の表示
     #* activity:HomeView内でimportする必要あり
@@ -15,6 +16,80 @@ from .forms import CategoryForm
 # TODO: カテゴリー毎のhome画面
     #* カテゴリー毎の累計時間・累計日数の表示
     #* グラフの表示に必要なjson型データの送信
+
+# class CategoryListView(LoginRequiredMixin, generic.View):
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'category_home.html')
+
+# class CategoryAjaxView(LoginRequiredMixin, generic.View):
+#     def get(self, request, *args, **kwargs):
+#         # 過去すべてのレコードを取得する
+#         end_date = date.min
+#         records = ActivityCategory.objects.filter(user=self.request.user, date__gte=end_date)
+
+#         # 日付ごとのレコードのリストを取得する
+#         records_by_date = {}
+#         for record in records:
+#             date_str = record.date.strftime('%-m/%-d')
+#             if date_str not in records_by_date:
+#                 records_by_date[date_str] = []
+#             records_by_date[date_str].append(record)
+
+#         # 日付のリストを作成する
+#         date_list = []
+#         # 日付から7日前までの日付を取得する
+#         current_date = date.today()
+#         for i in range(7):
+#             date_str = current_date.strftime('%-m/%-d')
+#             date_list.append(date_str)
+#             current_date -= timedelta(days=1)
+#         date_list.reverse()
+
+#         # 日付ごとのdurationを合計したリストを作成する
+#         duration_list = []
+#         for date_str in date_list:
+#             if date_str in records_by_date:
+#                 duration_sum = sum([record.duration for record in records_by_date[date_str]])
+#                 duration_list.append(duration_sum)
+#             else:
+#                 duration_list.append(0)
+
+#         # 日付の重複を除いた日数を計算する
+#         total_days = len(set(records_by_date.keys()))
+
+#         # 全レコードのdurationを合計する
+#         total_duration = sum([record.duration for record in records])
+
+#         # duration_listから各durationを'時間.分'の形式に変換する
+#         duration_list = [f"{duration // 60 + duration % 60 / 60:.1f}" for duration in duration_list]
+
+#         # JSON形式でデータを返す
+#         response_data = {
+#             'date_list': date_list,
+#             'duration_list': duration_list,
+#             'total_days': total_days,
+#             'total_duration': f"{total_duration // 60}:{total_duration % 60}"
+#         }
+#         return JsonResponse(response_data)
+
+
+class CategoryListView(LoginRequiredMixin, generic.ListView):
+    model = ActivityCategory
+    template_name = 'category_home.html'
+    context_object_name = 'activities_by_category'
+
+    def get_queryset(self):
+        return ActivityCategory.objects.filter(activity_record__user=self.request.user).order_by('category')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        activities_by_category = {}
+        for activity_category in context['object_list']:
+            if activity_category.category not in activities_by_category:
+                activities_by_category[activity_category.category] = []
+            activities_by_category[activity_category.category].append(activity_category.activity_record)
+        context['activities_by_category'] = activities_by_category
+        return context
 
 # カテゴリー作成機能
 class CategoryAddView(LoginRequiredMixin, generic.CreateView):
