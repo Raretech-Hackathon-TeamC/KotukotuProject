cdb427791544f889943cff9ea30ea91b3e85b2ae
DanK04 <kawazudan0430@gmail.com>
2023-05-16 04:52:44 +0900

    update graph.js

--- django/static/admin/js/graph.js
+++ django/static/admin/js/graph.js
@@ -1,65 +1,69 @@
-// データ
-const receivedData = {
- date_list: ["5/7", "5/8", "5/9", "5/10", "5/11", "5/12", "5/13"],
- category_duration_lists: {
-  1: ["1.0", "2.0", "1.5", "0.0", "2.0", "1.5", "2.0"],
-  2: ["0.0", "0.5", "1.0", "1.5", "1.0", "1.0", "0.5"],
-  3: ["1.0", "1.0", "1.0", "1.0", "1.0", "1.0", "1.0"]
- },
- uncategorized_duration_list: ["2.0", "1.5", "1.0", "2.0", "1.0", "1.5", "2.0"],
- total_days: 7,
- total_duration: "30:0",
- category_colors: {
-  1: "#DABEB7",
-  2: "#B7C1DA",
-  3: "#DAD4B7"
- }
-};
+// 非同期関数を定義します
+let barChart;  // barChartをグローバルスコープで宣言
 
-// データセットを作成
-const datasets = Object.keys(receivedData.category_duration_lists).map(categoryId => ({
- label: `Category ${categoryId}`,
- data: receivedData.category_duration_lists[categoryId].map(Number),
- backgroundColor: receivedData.category_colors[categoryId],
- borderColor: receivedData.category_colors[categoryId],
- borderWidth: 1
-}));
+(async () => {
+  try {
+    // '/activity/ajax_get_data/'というURLからデータを非同期で取得します
+    const response = await fetch('/activity/ajax_get_data/');
 
-// 未分類のデータセットを追加
-datasets.push({
- label: "Uncategorized",
- data: receivedData.uncategorized_duration_list.map(Number),
- backgroundColor: "#8AA7A1",
- borderColor: "#8AA7A1",
- borderWidth: 1
-});
+    // 取得したデータをJSON形式でパースします
+    const data = await response.json();
 
-// バーチャートデータを作成
-const barChartData = {
- labels: receivedData.date_list,
- datasets: datasets
-};
+    // データをコンソールにログとして出力します（デバッグ用）
+    console.log(data);
 
-const config = {
- type: "bar",
- data: barChartData,
- options: {
-  responsive: true,
-  maintainAspectRatio: false,
-  scales: {
-   y: {
-    beginAtZero: true,
-    stacked: true,
-    ticks: {
-     stepSize: 2,
-     max: 8
+    // HTMLの指定の要素に累計日数と累計時間を表示します
+    document.getElementById('totalDays').textContent = data.total_days;
+    document.getElementById('totalDuration').textContent = data.total_duration;
+
+    // 各カテゴリーごとにデータセットを作成します
+    // 'map'関数を使って各カテゴリーのデータセットを作り、それを'datasets'配列に追加します
+    const datasets = Object.keys(data.category_duration_lists).map(category => ({
+      label: category,  // カテゴリー名をラベルとして設定します
+      data: data.category_duration_lists[category].map(Number),  // 各カテゴリーのデータを数値として設定します
+      backgroundColor: data.category_colors[category],  // 各カテゴリーの色を背景色として設定します
+      borderColor: data.category_colors[category],  // 各カテゴリーの色をボーダー色として設定します
+      borderWidth: 1  // ボーダーの幅を1とします
+    }));
+
+    // バーチャートのデータを作成します
+    const barChartData = {
+      labels: data.date_list,  // データの日付をラベルとして設定します
+      datasets: datasets  // 先ほど作成したデータセットを設定します
+    };
+
+    // チャートの設定を作成します
+    const config = {
+      type: "bar",  // チャートのタイプを'bar'（バーチャート）とします
+      data: barChartData,  // 先ほど作成したバーチャートのデータを設定します
+      options: {
+        responsive: true,  // レスポンシブ対応をオンにします
+        scales: {
+          x: {
+            stacked: true,  // X軸（水平軸）を積み上げバーにします
+          },
+          y: {
+            beginAtZero: true,  // Y軸（垂直軸）の始点を0にします
+            stacked: true,  // Y軸を積み上げバーにします
+            tickes: {
+              stepSize: 2,
+              max: 8
+            }
+          }
+        }
+      }
+    };
+
+    // 既存のチャートが存在すれば破棄します
+    if (barChart) {
+      barChart.destroy();
     }
-   },
-   x: {
-    stacked: true // 縦に積み上げ
-   }
-  }
- }
-};
 
-const barChart = new Chart(document.getElementById("barChart"), config);
+    // チャートを作成します
+    barChart = new Chart(document.getElementById('barChart'), config);
+
+  } catch (error) {
+    // エラーが発生した場合はそれをコンソールにログとして出力します
+    console.error('Error:', error);
+  }
+})();  // 定義した非同期関数を実行します
