0ecd4c4bac719c8905d7f2d3868c47bdd998803b
hosomatu <kcfdmori0759@gmail.com>
2023-05-17 16:14:13 +0900

    ‘activity_listの連携’

--- django/static/admin/js/activity_list.js
+++ django/static/admin/js/activity_list.js
@@ -1,86 +1,94 @@
-// 受け取るデータの形がJSON形式でないため動かない。htmlの260行目で指定するエンドポイントでのデータをJSON形式にしないといけないかも。
-
-// JavaScriptで静的ファイルへのパスを通すための関数。djangoのやつを使わない（使えない）
+// JavaScriptへ静的ファイルへのパスを通すための関数。
 function static(path) {
  const staticRoot = "/static/"; // DjangoのSTATIC_URLに相当する部分
  return staticRoot + path;
 }
 
-// チャットアイテムをロードする非同期関数
-async function loadChatItems() {
- // チャットアイテムを表示するコンテナ要素を取得
- const container = document.getElementById("chat-container");
+// アクティビティレコードを取得する非同期関数
+async function fetchActivityRecords() {
+ try {
+  // '/activity/list/ajax_get_data/'というURLからデータを非同期で取得する。urls.pyを参照している。
+  const response = await fetch("/activity/list/ajax_get_data/");
 
- // fetchActivityRecords関数を実行し、アクティビティレコードを取得
- const records = await fetchActivityRecords();
+  // 取得したデータをJSON形式にする。
+  const data = await response.json();
 
- // レコードが空の場合、"リストがありません"と表示
- if (records.length === 0) {
-  const emptyMessage = document.createElement("p");
-  emptyMessage.textContent = "リストがありません";
-  container.appendChild(emptyMessage);
- } else {
-  // 各レコードに対して、チャットアイテムを作成し、コンテナに追加する
-  records.forEach(record => {
-   const chatItem = createChatItem(record);
-   container.appendChild(chatItem);
-  });
+  const container = document.getElementById("chat-container");
+
+  // データが存在しない場合のメッセージを表示
+  if (data.activities.length === 0) {
+   const emptyMessage = document.createElement("p");
+   emptyMessage.textContent = "リストがありません";
+   container.appendChild(emptyMessage);
+  } else {
+   // データが存在する場合、それぞれのデータに対してチャットアイテムを作成し、追加する
+   data.activities.forEach(record => {
+    const chatItem = createChatItem(record);
+    container.appendChild(chatItem);
+   });
+  }
+ } catch (error) {
+  console.error("Error:", error);
+  return null;
  }
 }
 
 // アクティビティレコードをもとにチャットアイテムを作成する関数
 function createChatItem(record) {
- // div要素を作成し、chat-bubbleクラスを追加
  const chatItem = document.createElement("div");
  chatItem.classList.add("chat-bubble");
+ chatItem.style.backgroundColor = record.category_color; // チャットの色を設定
 
- // チャットのテキストを作成し、アイテムに追加
- const chatText = document.createTextNode(`${record.day}は${record.time}時間頑張ったよ`);
+ const chatText = document.createTextNode(`${record.date}は${record.duration}時間頑張ったよ`);
  chatItem.appendChild(chatText);
 
- // img要素を作成し、属性とchat-imageクラスを追加
+ // メモのテキストを作成し、アイテムに追加
+ const memoText = document.createElement("p");
+ memoText.textContent = record.memo;
+ chatItem.appendChild(memoText);
+
+ //  亀の画像
  const chatImage = document.createElement("img");
  const kotukotuThumbnailPath = static("admin/img/kotukotu_thumbnail.png");
  chatImage.src = kotukotuThumbnailPath;
  chatImage.alt = "サムネイルのカメの画像";
  chatImage.classList.add("chat-image");
-
- // 画像要素をチャットアイテムに追加
  chatItem.appendChild(chatImage);
 
- // ゴミ箱ボタン要素を作成し、属性とクラスを追加
+ // ゴミ箱ボタン。押したらモーダルを動かす。
  const trashButton = document.createElement("button");
  trashButton.type = "button";
  trashButton.classList.add("trash-button");
 
- // ゴミ箱アイコン要素を作成し、属性とクラスを追加
  const trashIcon = document.createElement("img");
  const trashIconPath = static("admin/img/trash_icon.png");
  trashIcon.src = trashIconPath;
  trashIcon.alt = "ゴミ箱アイコン";
  trashIcon.classList.add("trash-icon");
 
- // ゴミ箱アイコン要素をゴミ箱ボタンに追加
  trashButton.appendChild(trashIcon);
-
- // ゴミ箱ボタン要素をチャットアイテムに追加
  chatItem.appendChild(trashButton);
 
- // ゴミ箱ボタンにイベントリスナーを追加
- trashButton.addEventListener("click", function () {
-  showModal();
+ //  htmlのモーダルから要素を取得
+ const deleteModal = document.getElementById("deleteModal");
+ const deleteForm = document.getElementById("deleteForm");
+ const cancelButton = document.getElementById("cancelButton");
+
+ //  jsで作成したtrashボタンに対してモーダル表示機能を追加。
+ trashButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  // ここで削除するアクティビティのIDをフォームのアクションにセット
+  deleteForm.action = `/activity/delete/${record.id}/`;
+  deleteModal.style.display = "block";
  });
 
- // 完成したチャットアイテムを返す
+ //  htmlから取得したキャンセルボタンに対してモーダル非表示機能を追加。
+ cancelButton.addEventListener("click", function () {
+  deleteModal.style.display = "none";
+ });
+
  return chatItem;
 }
 
-// アクティビティレコードを取得する非同期関数
-async function fetchActivityRecords() {
- // APIからデータを取得
- const response = await fetch(apiUrl);
- const data = await response.json();
-
- // データを返す
- return data;
-}
+// 非同期関数を呼び出す
+fetchActivityRecords();
