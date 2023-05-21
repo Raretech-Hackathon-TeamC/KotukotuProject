12280f55955289e33f9921190d55425838f2b08b
mappii130 <90373590+mappii130@users.noreply.github.com>
2023-05-21 23:36:40 +0900

    サインアップエラー表示

--- django/users/forms.py
+++ django/users/forms.py
@@ -16,12 +16,21 @@ class RegistForm(forms.ModelForm):
     
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data['password']
-        confirm_password = cleaned_data['confirm_password']
-        if password != confirm_password:
-            raise forms.ValidationError('パスワードが異なります')
-        # validate_passwordで画面遷移せずにエラーを表示
-        validate_password(self.cleaned_data['password'], self.instance)
+        password = cleaned_data.get('password')  # passwordの存在を確認する
+        confirm_password = cleaned_data.get('confirm_password')  # confirm_passwordの存在を確認する
+
+        if password and confirm_password:
+            if password != confirm_password:
+                self.add_error('confirm_password', 'パスワードが異なります')
+            try:
+                validate_password(password, self.instance)
+            except forms.ValidationError as error:
+                self.add_error('password', error)
+        else:
+            if not password:
+                self.add_error('password', 'パスワードを入力してください') # passwordが空の場合にエラーメッセージを追加
+            if not confirm_password:
+                self.add_error('confirm_password', '確認用パスワードを入力してください') # confirm_passwordが空の場合にエラーメッセージを追加
         
     def save(self, commit=False):
         user = super().save(commit=False)
